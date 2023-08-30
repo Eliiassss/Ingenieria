5965a04a754370408cf8a47b39a0e3a44ada2e21
Eliiassss <111454321+Eliiassss@users.noreply.github.com>
2023-08-30 15:01:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const calculateButton = document.getElementById("calculate");
-  const resultDiv = document.getElementById("result");
-  const kwhInput = document.getElementById("kwh");
-  const kwhForAnalysisSpan = document.getElementById("kwh-for-analysis");
-  const litersSpan = document.getElementById("liters");
-  const turbinesSpan = document.getElementById("turbines");
-  const timeSpan = document.getElementById("time");
-  const consumptionOptions = document.querySelectorAll(".consumption-option");
+const palabra1Input = document.getElementById("palabra1");
+const palabra2Input = document.getElementById("palabra2");
+const elegirBtn = document.getElementById("elegirBtn");
+const resultadoP = document.getElementById("resultado");
 
-  consumptionOptions.forEach(option => {
-    option.addEventListener("click", function () {
-      option.classList.toggle("selected");
-      updateKwhForAnalysis();
-    });
-  });
+elegirBtn.addEventListener("click", () => {
+  const palabra1 = palabra1Input.value;
+  const palabra2 = palabra2Input.value;
 
-  kwhInput.addEventListener("input", function () {
-    updateKwhForAnalysis();
-  });
-
-  calculateButton.addEventListener("click", function () {
-    const selectedOptions = document.querySelectorAll(".consumption-option.selected");
-    let totalKwh = parseFloat(kwhInput.value) || 0;
-
-    selectedOptions.forEach(option => {
-      totalKwh += parseFloat(option.getAttribute("data-kwh"));
-    });
-
-    calculateEnergy(totalKwh);
-  });
-
-  function updateKwhForAnalysis() {
-    const selectedOptions = document.querySelectorAll(".consumption-option.selected");
-    let totalKwh = parseFloat(kwhInput.value) || 0;
-
-    selectedOptions.forEach(option => {
-      totalKwh += parseFloat(option.getAttribute("data-kwh"));
-    });
-
-    kwhForAnalysisSpan.textContent = totalKwh;
-  }
-
-  function calculateEnergy(kwh) {
-    if (isNaN(kwh) || kwh <= 0) {
-      alert("Ingrese la cantidad de kWh requeridos para analizar");
-      return;
-    }
-
-    const requiredLiters = (kwh * 1713000) / 35000;
-    const requiredTurbines = Math.ceil(kwh / 12);
-    const timeHours = requiredTurbines * 24;
-
-    litersSpan.textContent = requiredLiters.toFixed(2) + " m³"; // Cambio "litros" por "m³"
-    turbinesSpan.textContent = requiredTurbines;
-    timeSpan.textContent = timeHours + " horas";
-
-    resultDiv.classList.remove("hidden");
+  if (palabra1 && palabra2) {
+    const palabras = [palabra1, palabra2];
+    const indiceAleatorio = Math.floor(Math.random() * palabras.length);
+    const palabraElegida = palabras[indiceAleatorio];
+    resultadoP.textContent = `Palabra elegida: ${palabraElegida}`;
+  } else {
+    resultadoP.textContent = "Por favor, ingresa ambas palabras.";
   }
 });
